50ad42299d9bf623738eeb53d5bf165419d8c12b
AtomicLotus3 <ghs_andre@hotmail.com>
2018-03-15 23:03:53 -0600

    Desarrollo
    Se realizaron cambios en el Layout Principal y se creó uno secundario a partir del primero. El secundario sirve para el funcionamiento dinámico del navbar.
    
    Se crearon las gestiones de Tipos y Categorías de productos.
    
    Se vinculó el proyecto a una base de datos para implementar Entity Framework.

---
+++ ToditoRentas/ToditoRentas/Controllers/PrincipalController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToditoRentas.Models;
+
+namespace ToditoRentas.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        ToditoRentasEntities db = new ToditoRentasEntities();
+
+        // GET: Principal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inicio()
+        {
+            try
+            {
+                ViewData["Categorias"] = db.Categoria.ToList();
+                return View();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return View();
+            }
+        }
+    }
+}
